069519821dcdd77023ec163d538f23d76c28f625
Felipe <felipe.monteiro2626@gmail.com>
2023-06-21 20:19:10 -0300

    feat(atividade): finalizando front so react pr ver

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
 
 import './App.css';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import AtividadeForm from './components/AtividadeForm';
 import Atividades from './components/Atividades';
-const initialState = [
-  {
-    id: 1,
-    prioridade: "2",
-    titulo: "titulo 1",
-    descricao: "Descansar"
-  },
-  {
-    id: 2,
-    prioridade: "2",
-    titulo: "titulo 2",
-    descricao: "Jogar futbol"
-  }
-]
+
 function App() {
   //observacao : Codigo para pegar maior de uma lista e adiciona 1 no input = value={Math.max.apply(Math,atividades.map((item) => item.id)) + 1}
-  const [atividades, setAtividades] = useState(initialState);
-  const[ativ,setAtiv] = useState({});
-  
-  function addAtividade(){
-    const atividade = {
-      id: Math.max.apply(Math,atividades.map((item) => item.id)) + 1,
-      prioridade: document.getElementById('prioridade').value,
-      titulo: document.getElementById('titulo').value,
-      descricao: document.getElementById('descricao').value
-    }
+  const [atividades, setAtividades] = useState([]);
+  const[ativ,setAtiv] = useState({id: 0});
+  const[index,setIndex] = useState(0);
+  useEffect(() =>{
+    atividades.length <= 0 ? setIndex(1) : setIndex(Math.max.apply(Math,atividades.map((item) => item.id)) + 1)
+  },[atividades])
+  function addAtividade(ativ){
     //criando Hook, cria um novo objeto e atualiza com um novo adicionado no formulario com o push ele não reconhece quando é atualizado
     //Expredo Operator: para que o estado seja alterado e mostrado quando atualizado
-    setAtividades([...atividades, { ...atividade }])
+    setAtividades([...atividades, { ...ativ, id: index}])
   }
   
   function deletarAtividade(id){
@@ -44,12 +28,21 @@ function App() {
     console.log(atividade);
     setAtiv(atividade[0])
   }
+  function atualizaAtividade(at){
+    setAtividades(atividades.map(item => item.id === at.id ? at : item))
+    setAtiv({id: 0})
+  }
+  function cancelarAtividade(){
+    setAtiv({id: 0})
+  }
   return (
     <>
       <AtividadeForm
         atividades={atividades}
         addAtividade={addAtividade}
         ativSelecionada={ativ}
+        atualizaAtividade={atualizaAtividade}
+        cancelarAtividade={cancelarAtividade}
       />
       <Atividades
         atividades={atividades}
--- src/components/AtividadeForm.jsx
+++ src/components/AtividadeForm.jsx
@@ -1,15 +1,52 @@
-import React, { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function AtividadeForm(props) {
-    const [atividade, setAtividade] = useState({})
+    const atividadeInicial = {
+        id: 0,
+        titulo: '',
+        prioridade: 0,
+        descricao: ''
+    }
+    const [atividade, setAtividade] = useState(atividadeAtual());
+    useEffect(() =>{
+        if(props.ativSelecionada.id !== 0){
+            setAtividade(props.ativSelecionada);
+        }
+    }, [props.ativSelecionada]);
+
     const inputTextHandler = (e) =>{
         const {name, value} = e.target;
         console.log(value);
         setAtividade({...atividade, [name]: value})
     }
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(props.ativSelecionada.id !== 0){
+            props.atualizaAtividade(atividade);
+        }else{
+            props.addAtividade(atividade);
+        }
+        setAtividade(atividadeInicial);
+    }
+    const handleCancelar =  (e) =>{
+        e.preventDefault();
+        props.cancelarAtividade();
+        setAtividade(atividadeInicial);
+        
+    }
     //Math.max.apply(Math,props.atividades.map((item) => item.id)) + 1
+    function atividadeAtual(){
+        if(props.ativSelecionada.id !== 0){
+            return props.ativSelecionada;
+        }else{
+            return atividadeInicial;
+        }
+    }
   return (
-    <form className='row g-3'>
+    <>
+    
+    <h1>Atividade {atividade.id !== 0 ? atividade.id : ''}</h1>
+    <form className='row g-3' onSubmit={handleSubmit}>
 
         <div className='col-md-6'>
           <label htmlFor='titulo' className='form-label'>Título</label>
@@ -26,13 +63,26 @@ export default function AtividadeForm(props) {
           </select>
         </div>
         <div className='col-md-12'>
-          <label htmlFor='descricao'  name='descricao' className='form-label'>Descrição</label>
-          <textarea id='descricao' type='text' className='form-control'placeholder="Descrição" onChange={inputTextHandler} value={atividade.descricao}/>
+          <label htmlFor='descricao' className='form-label'>Descrição</label>
+          <textarea id='descricao' name='descricao' type='text' className='form-control'placeholder="Descrição" onChange={inputTextHandler} value={atividade.descricao}/>
         </div>
         <hr/>
         <div className='col-12'>
-          <button onClick={props.addAtividade} type='button' className='btn btn-outline-secondary'>Adicionar atividade</button>
+            {
+                atividade.id === 0 ?
+                <button type='submit' className='btn btn-outline-secondary'>
+                    <i className='fas fa-plus me-2'></i>
+                    Adicionar atividade
+                </button>
+                :
+                <>
+                    <button type='submit' className='btn btn-outline-success me-3'>Salvar</button>
+                    <button onClick={handleCancelar} type='button' className='btn btn-outline-warning'>Cancelar</button>
+                </>
+            }
+          
         </div>
       </form>
+      </>
   )
 }
